465284ca851daee4c6106a294e0e766d61031392
DiegoArayaT <nachox341@gmail.com>
2025-03-20 11:00:58 -0300

    Complete Timestamp Microservice

--- index.js
+++ index.js
@@ -20,8 +20,27 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let { date } = req.params;
+
+  if(!date) {
+    date = new Date();
+  } else {
+    if (/^\d+$/.test(date)) {
+      date = parseInt(date);
+    }
+  }
+
+  let parsedDate = new Date(date);
+
+  if (parsedDate.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: parsedDate.getTime(),
+    utc: parsedDate.toUTCString(),
+  })
 });
 
 
